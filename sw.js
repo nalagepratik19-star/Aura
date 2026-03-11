093dd207cb75b3fa49a972cd0eccbc3c56a14942
nalagepratik19-star <nalagepratik19@gmail.com>
2026-03-11 07:39:44 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'habitOS-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/splash.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+  );
+});
+
+// Push notifications
+self.addEventListener('push', e => {
+  const data = e.data?.json() || { title: 'Habit OS', body: 'Time to check your habits!' };
+  e.waitUntil(self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: '/icons/icon-192x192.png',
+    badge: '/icons/icon-72x72.png',
+    vibrate: [200, 100, 200]
+  }));
+});
